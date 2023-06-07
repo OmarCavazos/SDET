97c2cb2e9fae92b124d4f81409e6a0ec1cdd14b4
Pato Mtz <pato_mtzc@outlook.com>
2023-06-06 19:01:38 -0600

    Session 4

---
+++ Session 4/challenge.md
@@ -0,0 +1,23 @@
+# Session 4 Challenge
+
+Usando lo que aprendimos en esta sesión, escribe la lógica necesaria para que la siguiente función imprima el número más grande entre los 3 proporcionados como parámetro:
+
+```
+function findLargest(num1, num2, num3) {
+    // Tu código aquí
+}
+```
+
+## Ejemplos:
+
+```
+findLargest(21,45,13) // 45
+```
+
+```
+findLargest(34,18,52) // 52
+```
+
+## Tips
+
+Para este ejercicio, solo necesitas los operadores de “>” y “&&”, pero siéntete libre de usar los operadores que consideres :)
---
+++ Session 4/if.js
@@ -0,0 +1,125 @@
+let myVar = "test" == "test";
+
+/* 
+if (myVar == true) {
+  console.log("True!");
+} else {
+  console.log("False!");
+}
+ */
+
+/*
+if (myVar == "true") {
+  console.log("True!");
+} else {
+  console.log("False!");
+}
+*/
+
+myVar = "true";
+
+/*
+if (myVar) {
+  console.log("True!");
+} else {
+  console.log("False!");
+}
+ */
+
+/*
+if (myVar === true) {
+  console.log("True!");
+} else {
+  console.log("False!");
+}
+*/
+
+myVar = 5;
+
+/*
+if (myVar) {
+  console.log("True!");
+} else {
+  console.log("False!");
+}
+*/
+
+/*
+if (myVar === "5") {
+  console.log("True!");
+} else {
+  console.log("False!");
+}
+*/
+
+/*
+if (myVar == "5") {
+  console.log("True!");
+} else {
+  console.log("False!");
+}
+*/
+
+/*
+if (myVar >= "5") {
+  console.log("True!");
+} else {
+  console.log("False!");
+}
+*/
+
+/*
+if (true) {
+  console.log("True!");
+} else {
+  console.log("False!");
+}
+*/
+
+/*
+if (5 > 5) {
+  console.log("True!");
+} else {
+  console.log("False!");
+}
+*/
+
+/*
+if (42) {
+  console.log("True!");
+} else {
+  console.log("False!");
+}
+*/
+
+/*
+if (0) {
+  console.log("True!");
+} else {
+  console.log("False!");
+}
+*/
+
+/*
+if ("") {
+  console.log("True!");
+} else {
+  console.log("False!");
+}
+*/
+
+/*
+if ({}) {
+  console.log("True!");
+} else {
+  console.log("False!");
+}
+*/
+
+/*
+if ([]) {
+  console.log("True!");
+} else {
+  console.log("False!");
+}
+*/
