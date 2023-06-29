db64d693b06a00a42dfc415a13da574358b3efb9
Omar Cavazos <omar_eternity92@hotmail.com>
2023-06-29 08:29:23 -0600

    extra challenge

--- Extra Challenges/challengeExtra.js
+++ Extra Challenges/challengeExtra.js
@@ -1,24 +1,40 @@
 function gradeProcessor(grades) {
-    for (let index = 0; index < grades.length; index++) {
-        if (grades[index] >= 90 && grades[index] <= 100) {
-            grades[index] = "A"
-        } else if (grades[index] >= 80 && grades[index] < 90)  {
-            grades[index] = "B"
-        } else if (grades[index] >= 70 && grades[index] < 80) {
-            grades[index] = "C"
-        } else if (grades[index] >= 60 && grades[index] < 70) {
-            grades[index] = "D"
-        } else if (grades[index] < 60 && grades[index] > 0) {
-            grades[index] = "F"
-        } else {
-            console.log(grades[index])
-            grades[index] = "error"
-        }
+    for (let index in grades) {
+        grades[index] =
+            (grades[index] >= 90 && grades[index] <= 100) ? "A" :
+            (grades[index] >= 80 && grades[index] < 90) ? "B" :
+            (grades[index] >= 70 && grades[index] < 80) ? "C" :
+            (grades[index] >= 60 && grades[index] < 70) ? "D" :
+            (grades[index] < 60 && grades[index] > 0) ? "F" : "error";
         
         console.log(grades[index])
     }
     console.log(grades)
 }
+
+
+
+// function gradeProcessor(grades) {
+//     for (let index = 0; index < grades.length; index++) {
+//         if (grades[index] >= 90 && grades[index] <= 100) {
+//             grades[index] = "A"
+//         } else if (grades[index] >= 80 && grades[index] < 90)  {
+//             grades[index] = "B"
+//         } else if (grades[index] >= 70 && grades[index] < 80) {
+//             grades[index] = "C"
+//         } else if (grades[index] >= 60 && grades[index] < 70) {
+//             grades[index] = "D"
+//         } else if (grades[index] < 60 && grades[index] > 0) {
+//             grades[index] = "F"
+//         } else {
+//             console.log(grades[index])
+//             grades[index] = "error"
+//         }
+        
+//         console.log(grades[index])
+//     }
+//     console.log(grades)
+// }
 
 
 
