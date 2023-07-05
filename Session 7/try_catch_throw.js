0f61d74051628938296afea35b1469279031712d
Pato Mtz <pato_mtzc@outlook.com>
2023-07-05 17:59:02 -0600

    Session 7

---
+++ Session 7/try_catch_throw.js
@@ -0,0 +1,29 @@
+function producesAnError() {
+  throw "This Failed miserably!";
+}
+
+function alsoProducesAnError() {
+  console.log(nonExistentVariable.split());
+}
+
+function process() {
+  try {
+    // Something
+    alsoProducesAnError();
+  } catch (e) {
+    console.log("Error: " + e);
+  } finally {
+    // Do producesAnError();
+  }
+
+  try {
+    // Something
+    producesAnError();
+  } catch (e) {
+    console.log("Error: " + e);
+  } finally {
+    producesAnError();
+  }
+}
+
+process();
