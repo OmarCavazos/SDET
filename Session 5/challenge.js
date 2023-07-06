466913bd2d3d7a2b993585129d2a023d7ec5dc04
Omar Cavazos <omar_eternity92@hotmail.com>
2023-07-05 18:23:06 -0600

    challengeDados

--- Session 5/challenge.js
+++ Session 5/challenge.js
@@ -1,8 +1,10 @@
 const vidaEnemigo = 200;
 
+// var randomNumber = Math.floor(Math.random() * 6) + 1;
 /**
  * Regresa un número entre el 1 y el 6
  */
+
 function tirarDado() {
   var randomNumber = Math.floor(Math.random() * 6) + 1;
   return randomNumber;
@@ -10,20 +12,32 @@ function tirarDado() {
 
 function golpe(resultadoDados) {
   let puntosDeDaño;
-
+  console.log(resultadoDados)
+  if (resultadoDados > 3) {
+    puntosDeDaño = 60
+  } else {
+    puntosDeDaño = 20
+  }
   /**
    * TU CODIGO AQUI!!!
    * Si al aventar los dados:
    * dados mayor a 3 = 60 puntos de daño
    * dados menor a 3 = 20 puntos de daño
-   */
-
+  */
   return puntosDeDaño;
 }
+// console.log(golpe)
+
 
 function patada(resultadoDados) {
   let puntosDeDaño;
-
+  if (resultadoDados >= 5) {
+    puntosDeDaño = 60
+  } else if (resultadoDados == 3 || resultadoDados == 4) {
+    puntosDeDaño = 40
+  } else {
+    puntosDeDaño = 20
+  }
   /**
    * TU CODIGO AQUI!!!
    * Si al aventar los dados:
@@ -34,6 +48,7 @@ function patada(resultadoDados) {
 
   return puntosDeDaño;
 }
+console.log(patada())
 
 /**
  * El jugador puede atacar con una patada y un golpe por turno.
@@ -50,8 +65,15 @@ for (let turno = 0; turno < 3; turno++) {
    * ejecuta la función de golpe() y patada(), utilizando los valores de dado1 y dado2
    * suma el daño generado durante los 3 turnos entre ambos ataques
    * imprime el daño por golpe, por patada y la suma por turno
-   */
+  */
 
+  dado1 = tirarDado()
+  dado2 = tirarDado()
+  let dañoGolpe = golpe(dado1)
+  let dañoPatada = patada(dado2)
+  let dañoTurno = dañoGolpe + dañoPatada
+  dañoTotal = dañoTotal + dañoTurno
+  
   console.log(`
   Dado1: ${dado1},
   Dado2: ${dado2},
@@ -61,13 +83,28 @@ for (let turno = 0; turno < 3; turno++) {
   `);
 }
 
+console.log(dañoTotal)
+console.log(vidaEnemigo)
+console.log(dañoTotal)
+
 const vidaRestante = vidaEnemigo - dañoTotal;
+console.log(vidaRestante)
 
 console.log(`
   Daño Total: ${dañoTotal}
   Vida Total: ${vidaEnemigo}
   Vida Restante: ${vidaRestante}
   `);
+
+function Resultado(vidaRestante) {
+  console.log(vidaRestante)
+  if (vidaRestante <= 0) {
+    return ("jugador gano")
+  } else {
+    return ("jugador perdio")
+  }
+}
+console.log(Resultado(vidaRestante))
 
 /**
  * Imprime el resultado de la batalla!
